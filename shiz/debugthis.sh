38af3d54ecf7acece737845cfc9cc798bd71fcdd
vladus2000 <docker@matt.land>
2018-05-27 16:57:18 -0500

    adding call to extradebug if a container has it

--- shiz/debugthis.sh
+++ shiz/debugthis.sh
@@ -5,6 +5,11 @@ rm /etc/apt/apt.conf.d/docker-clean
 apt-get install -y iproute2 vim wget bash-completion
 echo . /etc/bash_completion >> /etc/bash.bashrc
 echo . /bashrc >> /etc/bash.bashrc
+
+if [ -f /extradebug.sh ]; then
+	/extradebug.sh
+fi
+
 echo
 echo restart bash for completion
 echo
